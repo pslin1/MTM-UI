f92a9b0fedb6bee5b07aa86bc716747d2902536c
Jacqueline Forest <jrforest@cs.ualberta.ca>
2018-01-24 13:28:14 -0700

    implement about

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,19 @@ export default class About extends React.Component {
   
   render() {
     return (
-      <div></div>
+      <div>
+        <div className = 'container'>
+          <div className = 'row'>
+            <div className = 'col-lg-12 text-center'>
+              <h2>About</h2>
+              </div>
+              <div className = 'col-lg-12 text-center'>
+                <h2>You can find more information here: serranos@ualberta.ca</h2>
+                </div>
+                </div>
+                </div>
+      </div>
+
     );
   }
   
